e849e0e65b3bccdd1e5ec4284b2cfa963942e4e7
andrewcpeltier <andrew_peltier@my.uri.edu>
2018-02-20 10:38:16 -0500

    Working Bluetooth
    This version connects, disconnects, and writes (just the word "Hey") to a paired Bluetooth device.

---
+++ Software_Engineering_305/app/src/main/java/com/example/android/software_engineering_305/activities/SettingsActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.software_engineering_305.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.software_engineering_305.R;
+import com.example.android.software_engineering_305.services.BluetoothService;
+
+/**
+ * Created by Andrew on 2/19/18.
+ */
+
+public class SettingsActivity extends AppCompatActivity
+{
+    private static final String TAG = "SettingsActivity";
+    private Context mContext;
+    private Button sendButton;
+
+    /**         --onCreate(...)--
+     *
+     * This is where the settings for the app are going to be read/changed.
+     * Right now, all you can do is say hi.
+     *
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Sets up the activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        mContext = this;
+
+        sendButton = findViewById(R.id.write_btn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothService.write(mContext, "Hey");
+            }
+        });
+    }
+
+    /**                     --onBackPressed()--
+     *  Returns to the parent activity (ScanActivity) if the back button is pressed
+     *  Also, disconnects from the current device
+     */
+    @Override
+    public void onBackPressed()
+    {
+        BluetoothService.disconnect(mContext);
+        Log.e(TAG, "Back pressed. Navigating to " + getParentActivityIntent());
+        Intent intent = this.getParentActivityIntent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        super.onBackPressed();
+    }
+}

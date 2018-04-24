3836352d560a5e14c31f4dfa63e0b74e5be819ea
jdsteven <jdsteven7@gmail.com>
2018-04-24 14:07:48 -0400

    Only list the scarecrow devices on scan list

--- Software_Engineering_305/app/src/main/java/com/example/android/software_engineering_305/activities/ScanActivity.java
+++ Software_Engineering_305/app/src/main/java/com/example/android/software_engineering_305/activities/ScanActivity.java
@@ -157,9 +157,10 @@ public class ScanActivity extends AppCompatActivity
             for(BluetoothDevice device : pairedDevices) {
                 HashMap<String,String> btDevice = new HashMap<>(); //need to use a hashmap for the multiline display
                 Log.i(TAG, "Paired device found: " + device.getAddress());
-                btDevice.put("name",device.getName());
-                btDevice.put("address",device.getAddress());
-                myList.add(btDevice);
+                if(device.getName().substring(0,3).equals("LS-")) {
+                    btDevice.put("name", device.getName());
+                    btDevice.put("address", device.getAddress());
+                    myList.add(btDevice);
             }
         }
     }
